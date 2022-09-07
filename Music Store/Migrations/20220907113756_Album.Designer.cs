f4abfa1cff9b66994b356d74d431ec888c0e4623
lucastran007 <lucastran007@gmail.com>
2022-09-07 13:57:48 +0200

    Tilføjet albumimage til album migraion

--- Music Store/Migrations/20220907113756_Album.Designer.cs
+++ Music Store/Migrations/20220907113756_Album.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Music_Store.Migrations
 {
     [DbContext(typeof(DbContext))]
-    partial class DbContextModelSnapshot : ModelSnapshot
+    [Migration("20220907113756_Album")]
+    partial class Album
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Music Store/Migrations/20220907113756_Album.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Music_Store.Migrations
+{
+    public partial class Album : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AlbumArtUrl",
+                table: "Album");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AlbumImage",
+                table: "Album",
+                type: "nvarchar (100)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AlbumImage",
+                table: "Album");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AlbumArtUrl",
+                table: "Album",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
